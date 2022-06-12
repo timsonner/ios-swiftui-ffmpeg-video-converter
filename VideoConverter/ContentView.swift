92daeefce8458691b02b157ccefb80f1755c9e31
timsonner <66705347+timsonner@users.noreply.github.com>
2022-06-11 18:31:08 -0600

    Initial Commit

---
+++ VideoConverter/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  VideoConverter
+//
+//  Created by Timothy Sonner on 6/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VideoConverter/VideoConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoConverterApp.swift
+//  VideoConverter
+//
+//  Created by Timothy Sonner on 6/11/22.
+//
+
+import SwiftUI
+
+@main
+struct VideoConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
